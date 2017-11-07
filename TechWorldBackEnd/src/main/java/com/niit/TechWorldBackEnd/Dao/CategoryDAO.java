ce79d5158830253e6ea5d55f50741989ce1af40e
user <surendar8rrs@gmail.com>
2017-11-07 13:57:27 +0530

    categorydaotestcase

---
+++ TechWorldBackEnd/src/main/java/com/niit/TechWorldBackEnd/Dao/CategoryDAO.java
@@ -0,0 +1,29 @@
+package com.niit.TechWorldBackEnd.Dao;
+
+import java.util.List;
+
+import com.niit.TechWorldBackEnd.model.Category;
+
+public interface CategoryDAO {
+	
+	
+	public boolean save(Category category);
+	
+
+	public boolean update(Category category);
+	
+	
+	
+	public List<Category> list();
+	
+	
+	public Category getCategoryById(String id);
+	
+	
+	public boolean delete(String id);
+	
+
+	
+	public Category getCategoryByName(String name);
+
+}
---
+++ TechWorldBackEnd/src/test/java/com/niit/TechWorldBackEnd/CategoryDAOTestCase.java
@@ -0,0 +1,81 @@
+package com.niit.TechWorldBackEnd;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.TechWorldBackEnd.Dao.CategoryDAO;
+import com.niit.TechWorldBackEnd.model.Category;
+
+
+
+public class CategoryDAOTestCase {
+
+	@Autowired
+	static AnnotationConfigApplicationContext context;
+
+	@Autowired
+	static CategoryDAO categoryDAO;
+
+	@Autowired
+	static Category category;
+
+	@BeforeClass
+	public static void initialize() {
+		context = new AnnotationConfigApplicationContext();
+		context.scan("com.niit.TechWorldBackEnd");
+		context.refresh();
+
+		categoryDAO = (CategoryDAO) context.getBean("categoryDAO");
+		
+
+
+		category = (Category) context.getBean("category");
+
+	}
+
+	
+	/*@Test
+	public void createCategoryTestCase() {
+		category.setId("Mobiles");
+		category.setName("Mobiles");
+		category.setDescription("This category contains Top Mobiles");
+
+		boolean flag = categoryDAO.save(category);
+
+		assertEquals("createCategoryTestCase", true, flag);
+
+	}
+	
+*/
+	/*@Test
+	public void updateCategoryTestCase() {
+		category.setId("Mobiles");
+		category.setName("Mobiles");
+		category.setDescription("This category contains smart phones");
+
+		boolean flag = categoryDAO.update(category);
+
+		assertEquals("updateCategoryTestCase", true, flag);
+
+	}
+
+	@Test
+	public void deleteCategoryTestCase() {
+		category.setName("Mobiles");
+		boolean flag = categoryDAO.delete(category.getName());
+
+		assertEquals("deleteCategoryTestCase", true, flag);
+
+	}
+*/
+/*	@Test
+	public void listAllCategoryTestCase() {
+		int actualSize = categoryDAO.list().size();
+		assertEquals(6, actualSize);
+	}
+*/
+}
